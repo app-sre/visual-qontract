8d885209619a78c78438b9a9f12145ff5c7e9d2c
sdlhopol <sdlhopol@redhat.com>
2019-06-06 15:52:38 -0400

    fix style problems

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { OptionsMenuItem, OptionsMenu, OptionsMenuToggle, Grid, GridItem } from '@patternfly/react-core';
-import { red } from 'ansi-colors';
 
 function SearchBar({ filterText, handleFilterTextChange, handleSelect, options, selected }) {
+  let optionsMenu;
   const [isOpen, toggle] = useState(false);
   function handleChange(e) {
     handleFilterTextChange(e.target.value);
@@ -21,25 +21,38 @@ function SearchBar({ filterText, handleFilterTextChange, handleSelect, options, 
     const { text } = toggleTemplateProps;
     return <React.Fragment>{text}</React.Fragment>;
   }
-  const menuItems = options.map(i => (
-    <OptionsMenuItem onSelect={onSelect} isSelected={selected === i} id={i} key={i}>
-      {i}
-    </OptionsMenuItem>
-  ));
-  const menuToggle = (
-    <OptionsMenuToggle onToggle={onToggle} toggleTemplate={toggleTemplate} toggleTemplateProps={{ text: selected }} />
-  );
+  if (options !== undefined) {
+    const menuItems = options.map(i => (
+      <OptionsMenuItem onSelect={onSelect} isSelected={selected === i} id={i} key={i} className="optionsMenuItem">
+        {i}
+      </OptionsMenuItem>
+    ));
+    const menuToggle = (
+      <OptionsMenuToggle onToggle={onToggle} toggleTemplate={toggleTemplate} toggleTemplateProps={{ text: selected }} />
+    );
+    optionsMenu = (
+      <OptionsMenu
+        id="options-menu"
+        menuItems={menuItems}
+        isOpen={isOpen}
+        toggle={menuToggle}
+        className="optionsMenu"
+      />
+    );
+  } else {
+    optionsMenu = "";
+  }
   return (
-    <Grid gutter="md">
-      <GridItem span={1}>
-        <OptionsMenu id="options-menu" menuItems={menuItems} isOpen={isOpen} toggle={menuToggle} />
-      </GridItem>
-      <GridItem span={6}>
-        <form>
-          <input type="text" placeholder="Search..." value={filterText} onChange={handleChange} />
-        </form>
-      </GridItem>
-    </Grid>
+    <div className="searchBar">
+      <Grid gutter="md">
+        <GridItem span={2}>
+          <form>
+            <input className="search" type="text" placeholder="Search..." value={filterText} onChange={handleChange} />
+          </form>
+        </GridItem>
+        <GridItem span={1}> {optionsMenu} </GridItem>
+      </Grid>
+    </div>
   );
 }
 
