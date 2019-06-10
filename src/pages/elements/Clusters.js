b1bb1280a5d87bfc3987a283925f3f86512af888
sdlhopol <sdlhopol@redhat.com>
2019-06-10 09:33:01 -0400

    add search bar to cluster page

--- src/pages/elements/Clusters.js
+++ src/pages/elements/Clusters.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { chunk } from 'lodash';
 import { CardGrid, Row, Col, Card, CardHeading, CardBody, CardFooter, CardTitle } from 'patternfly-react';
 import { sortByName } from '../../components/Utils';
+import SearchBar from '../../components/SearchBar';
 
 function Clusters({ clusters }) {
   // cardsWidth * cardsPerRow must be <= 12 (bootstrap grid)
   const cardWidth = 4;
   const cardsPerRow = 3;
+  const [selected, changeSelected] = useState('Name');
+  const [filterText, changeFilterText] = useState('');
+  const matchedClusters = [];
+  let i;
+  for (i = 0; i < clusters.length; i++) {
+    if (selected === 'Name') {
+      if (clusters[i].name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1) {
+        matchedClusters[matchedClusters.length] = clusters[i];
+      }
+    }
+  }
+  function handleFilterTextChange(txt) {
+    changeFilterText(txt);
+  }
 
-  const rows = chunk(sortByName(clusters), cardsPerRow).map(c => (
+  function handleSelect(newSelection) {
+    changeSelected(newSelection);
+  }
+
+  const rows = chunk(sortByName(matchedClusters), cardsPerRow).map(c => (
     <Row key={c[0].path}>
       {c.map(s => (
         <Col xs={cardWidth} key={s.path}>
@@ -41,7 +60,17 @@ function Clusters({ clusters }) {
     </Row>
   ));
 
-  return <CardGrid matchHeight>{rows}</CardGrid>;
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        handleFilterTextChange={handleFilterTextChange}
+        handleSelect={handleSelect}
+        selected={selected}
+      />
+      <CardGrid matchHeight>{rows}</CardGrid>
+    </div>
+  );
 }
 
 export default Clusters;
