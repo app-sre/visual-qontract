aa7ef0f76929d02b4ccad01370bee4f18b5405dd
sdlhopol <sdlhopol@redhat.com>
2019-07-11 15:19:40 -0400

    fragment types

--- src/fragmentTypesGenerator.js
+++ src/fragmentTypesGenerator.js
@@ -18,15 +18,13 @@ fetch('http://localhost:4000/graphql', {
           }
         }
       }
-    `,
-  }),
+    `
+  })
 })
   .then(result => result.json())
   .then(result => {
     // here we're filtering out any type information unrelated to unions or interfaces
-    const filteredData = result.data.__schema.types.filter(
-      type => type.possibleTypes !== null,
-    );
+    const filteredData = result.data.__schema.types.filter(type => type.possibleTypes !== null);
     result.data.__schema.types = filteredData;
     fs.writeFileSync('./src/fragmentTypes.json', JSON.stringify(result.data), err => {
       if (err) {
