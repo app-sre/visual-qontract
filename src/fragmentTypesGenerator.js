4b0dcde3099c66986f4fa5c376ab32ffd6b94811
sdlhopol <sdlhopol@redhat.com>
2019-07-12 14:42:39 -0400

    delete fragmentTypes.json and add line to entrypoint

--- src/fragmentTypesGenerator.js
+++ src/fragmentTypesGenerator.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-fetch('http://localhost:4000/graphql', {
+fetch(process.env.GRAPHQL_URI || 'http://localhost:4000/graphql', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
