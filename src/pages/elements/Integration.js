4c56a7aa24884a2ae1dc1a84d54eb018ff72fe76
sdlhopol <sdlhopol@redhat.com>
2019-08-12 14:06:46 -0400

    markdown renderer

--- src/pages/elements/Integration.js
+++ src/pages/elements/Integration.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Markdown from 'react-markdown';
 import Definition from '../../components/Definition';
 
 function Integration({ integration }) {
+  console.log(integration.usage);
   return (
     <React.Fragment>
       <h4>Info</h4>
@@ -26,7 +28,7 @@ function Integration({ integration }) {
       <h4>Description</h4>
       {integration.description}
       <h4> Usage </h4>
-      {integration.usage}
+      <Markdown source={integration.usage} />
     </React.Fragment>
   );
 }
