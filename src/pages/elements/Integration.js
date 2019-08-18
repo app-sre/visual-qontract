e6716460c074a8db586f9a34383e5fe02c331814
Sydney Dlhopolsky <41091194+SydneyDlh@users.noreply.github.com>
2019-08-18 01:47:00 -0400

    Integration pages (#23)

---
+++ src/pages/elements/Integration.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Definition from '../../components/Definition';
+
+function Integration({ integration }) {
+  const usage = integration.usage.split('\n').map((i, key) => <div key={key}>{i}</div>);
+  return (
+    <React.Fragment>
+      <h4>Info</h4>
+      <Definition
+        items={[
+          ['Integration', integration.name],
+          [
+            'Path',
+            <a href={`${window.DATA_DIR_URL}${integration.path}`} target="_blank" rel="noopener noreferrer">
+              {integration.path}
+            </a>
+          ],
+          [
+            'Upstream',
+            <a href={integration.upstream} target="_blank" rel="noopener noreferrer">
+              {integration.upstream}
+            </a>
+          ],
+          ['Short Description', integration.shortDescription]
+        ]}
+      />
+      <h4>Description</h4>
+      {integration.description}
+      <h4> Usage </h4>
+      {usage}
+    </React.Fragment>
+  );
+}
+
+export default Integration;
