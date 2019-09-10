73bd050a4bb9bcd2fa9727ca64fd7eed83b96d9f
Maor Friedman <maor.friedman@redhat.com>
2019-09-10 09:02:23 +0300

    add aws accounts pages

---
+++ src/pages/elements/AWSAccount.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Definition from '../../components/Definition';
+
+function AWSAccount({ awsaccount }) {
+  const ac = awsaccount;
+  return (
+    <React.Fragment>
+      <h4>Info</h4>
+      <Definition
+        items={[
+          ['Account', ac.name],
+          [
+            'Path',
+            <a href={`${window.DATA_DIR_URL}${ac.path}`} target="_blank" rel="noopener noreferrer">
+              {ac.path}
+            </a>
+          ],
+          [
+            'Console URL',
+            <a href={ac.consoleUrl} target="_blank" rel="noopener noreferrer">
+              {ac.consoleUrl}
+            </a>
+          ]
+        ]}
+      />
+
+      <h4>Description</h4>
+      {ac.description}
+    </React.Fragment>
+  );
+}
+
+export default AWSAccount;
