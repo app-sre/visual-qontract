035226849316c14fef1f756ccab2d3f5d57f0918
Violet Yao <vyao@redhat.com>
2020-07-20 21:50:06 -0700

    refactor & only allow email recepients for now

--- src/components/NotificationSelect.js
+++ src/components/NotificationSelect.js
@@ -4,7 +4,61 @@ import CreatableSelect from 'react-select/creatable';
 import {components} from "react-select";
 import {RED, GREY} from "./ColourStyles.js";
 
-const NotificationSelect = props => {
+
+const SelectAll = props => {
+  if (props.allowSelectAll) {
+    return (
+      <CreatableSelect
+        {...props}
+        options={[props.allOption, ...props.options]}
+        onChange={(selected, event) => {
+          if (selected !== null && selected.length > 0) {
+            if (selected[selected.length - 1].value === props.allOption.value) {
+              return props.onChange([props.allOption, ...props.options]);
+            }
+            let result = [];
+            if (selected.length === props.options.length) {
+              if (selected.includes(props.allOption)) {
+                result = selected.filter(
+                  option => option.value === props.allOption.value
+                );
+              } else if (event.action === "select-option") {
+                result = [props.allOption, ...props.options];
+              }
+
+              return props.onChange(result);
+            }
+          }
+
+          return props.onChange(selected);
+        }}
+      />
+    );
+  }
+
+  return <CreatableSelect {...props} />;
+};
+
+SelectAll.propTypes = {
+  options: PropTypes.array,
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  allowSelectAll: PropTypes.bool,
+  allOption: PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+    contact: PropTypes.string,
+  })
+};
+
+SelectAll.defaultProps = {
+  allOption: {
+    label: "Select all affected",
+    value: "*",
+  }
+};
+
+const SelectService = props => {
   if (props.allowSelectAll) {
     return (
       <CreatableSelect
@@ -37,7 +91,7 @@ const NotificationSelect = props => {
   return <CreatableSelect {...props} />;
 };
 
-NotificationSelect.propTypes = {
+SelectService.propTypes = {
   options: PropTypes.array,
   value: PropTypes.any,
   onChange: PropTypes.func,
@@ -49,7 +103,7 @@ NotificationSelect.propTypes = {
   })
 };
 
-NotificationSelect.defaultProps = {
+SelectService.defaultProps = {
   allOption: {
     label: "Select all affected",
     value: "*",
@@ -77,82 +131,4 @@ const MultiValue = props => (
   </components.MultiValue>
 );
 
-const SelectService = props => {
-  return (
-    <NotificationSelect
-      {...props}
-    />
-  );
-};
-
-SelectService.defaultProps = {
-  allOption: {
-    label: "Select all affected services",
-    value: "*"
-  }
-};
-
-const SelectRecepient = props => {
-  return (
-    <NotificationSelect
-      {...props}
-      options={[props.allOption, props.allEmailOption, props.allSlackOption, ...props.options]}
-      onChange={(selected, event) => {
-        if (selected !== null && selected.length > 0) {
-          if (selected[selected.length - 1].value === props.allOption.value) {
-            return props.onChange([props.allOption, props.allEmailOption, props.allSlackOption, ...props.options]);
-          }
-          if (selected[selected.length - 1].value === props.allEmailOption.value) {
-            return props.onChange([props.allEmailOption, ...props.options.filter(option => option.contact === "Email")]);
-          }
-          if (selected[selected.length - 1].value === props.allSlackOption.value) {
-            return props.onChange([props.allSlackOption, ...props.options.filter(option => option.contact === "Slack")]);
-          }
-          let result = [];
-          if (selected.length === props.options.filter(option => option.contact !== "Email").length) {
-            if (selected.includes(props.allEmailOption)) {
-              result = selected.filter(
-                option => option.value !== props.allEmailOption.value
-              );
-            } else if (event.action === "select-option") {
-              result = [props.allEmailOption, ...props.options];
-            }
-            return props.onChange(result);
-          }
-          if (selected.length === props.options.filter(option => option.contact !== "Slack").length) {
-            if (selected.includes(props.allSlackOption)) {
-              result = selected.filter(
-                option => option.value !== props.allSlackOption.value
-              );
-            } else if (event.action === "select-option") {
-              result = [props.allSlackOption, ...props.options];
-            }
-            return props.onChange(result);
-          }
-        }
-
-        return props.onChange(selected);
-      }}
-    />
-  );
-};
-
-SelectRecepient.defaultProps = {
-  allOption: {
-    label: "Select all affected recepients - Email + Slack",
-    value: "**",
-    contact: "Email + Slack"
-  },
-  allEmailOption: {
-    label: "Select all affected recepients - Email",
-    value: "***",
-    contact: "Email"
-  },
-  allSlackOption: {
-    label: "Select all affected recepients - Slack",
-    value: "****",
-    contact: "Slack"
-  }
-};
-
-export {NotificationSelect, SelectService, SelectRecepient, Option, MultiValue};
+export {SelectAll, SelectService, Option, MultiValue};
--- src/pages/elements/NewNotification.js
+++ src/pages/elements/NewNotification.js
@@ -9,8 +9,8 @@ import { Link } from 'react-router-dom';
 import makeAnimated from 'react-select/animated';
 import CreatableSelect from 'react-select/creatable';
 import Page from '../../components/Page';
-import { SelectService, SelectRecepient, Option, MultiValue } from "../../components/NotificationSelect";
-import { sortByLabel, sortByValue} from '../../components/Utils';
+import { SelectAll, SelectService, Option, MultiValue } from "../../components/NotificationSelect";
+import { sortByValue} from '../../components/Utils';
 import { RED, GREY, ColourStyles} from '../../components/ColourStyles';
 import {
   Form,
@@ -27,16 +27,8 @@ const client = new ApolloClient({
   uri: window.GRAPHQL_URI,
 });
 
-const GET_SERVICE_NAMES = gql`
-  query ServiceNames{
-    apps_v1 {
-      name
-    }
-  }
-`;
-
-const GET_SERVICE_OWNERS = gql`
-  query GetServiceOwners{
+const GET_SERVICE_INFO = gql`
+  query GetServiceOwners {
     apps_v1 {
       path
       name
@@ -66,19 +58,8 @@ const GET_USERS = gql`
   }
 `;
 
-const GET_DEPENDENCIES = gql`
-  query Dependencies{
-    apps_v1 {
-      name
-      dependencies {
-        name
-      }
-    }
-}
-`;
-
 const GET_DEPENDENCIES_LIST = gql`
-  query Dependencies{
+  query GetDependencies {
     dependencies_v1 {
       name
     }
@@ -87,7 +68,6 @@ const GET_DEPENDENCIES_LIST = gql`
 
 var user_dic = {};
 var dependency_dic = {};
-var all_services = [];
 
 client
   .query({
@@ -97,28 +77,14 @@ client
     for (var user of result.data.users_v1) {
       var dic = {};
       dic['path'] = user.path;
-      if (user.slack_username) {
-        dic['slack'] = user.slack_username;
-      } else {
-        dic['slack'] = user.org_username;
-      }
       user_dic[user.name] = dic;
     }
   });
 
-client
-  .query({
-    query: GET_SERVICE_NAMES
-  })
-  .then(result => {
-    for (var service of result.data.apps_v1) {
-      all_services.push(service.name);
-    }
-  });
 
 client
   .query({
-    query: GET_DEPENDENCIES
+    query: GET_SERVICE_INFO
   })
   .then(result => {
     for (var service of result.data.apps_v1) {
@@ -141,11 +107,9 @@ class NewNotification extends React.Component {
       selectedDependencies: [{"value": "None", "label": "None"}],
       selectedServices: [],
       selectedUsers: [],
-      selectedSlackUsers: [],
       jira: '',
       short_description: '',
-      long_description: '',
-      validated: 'error'
+      long_description: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.options = [
@@ -153,26 +117,19 @@ class NewNotification extends React.Component {
       { value: 'Maintenance', label: 'Maintenance', disabled: false },
       { value: 'Info', label: 'Info', disabled: false }
     ];
-    this.user_path = [];
-    this.user_dic = {};
     this.selected_service_names = [];
-    this.dependency = [];
   }
 
   handleSubmit(event) {
-    var dependencies = [];
-    var services = [];
+    var dependency_names = [];
+    var service_names = [];
     var users = [];
     var users_str = '';
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    var date = d.getDate();
     for (var s of this.state.selectedDependencies) {
-      dependencies.push(s.value);
+      dependency_names.push(s.value);
     }
     for (var s of this.state.selectedServices) {
-      services.push(s.value);
+      service_names.push(s.value);
     }
     for (var u of this.state.selectedUsers) {
       if (user_dic.hasOwnProperty(u.value)){
@@ -187,17 +144,16 @@ class NewNotification extends React.Component {
     + "• Notification Type: " + this.state.notification_type + "\n"
     + "• Recipients (Email): " + users_str + "\n"
     + "• #Jira: " + this.state.jira + "\n"
-    + '• Short Description: ' + this.state.short_description + "\n"
-    + '• Description: ' + this.state.long_description + "\n"
+    + "• Short Description: " + this.state.short_description + "\n"
+    + "• Description: " + this.state.long_description + "\n"
     + "========================= " + "\n"
     + "Are you sure to send the notification? " + "\n"
     + "========================= " + "\n");
     if (r) {
       const notification = {'notification_type': this.state.notification_type,
                             'labels': '{}',
-                            'date': year + '-' + month + '-' + date,
-                            'affected_dependencies': dependencies,
-                            'affected_services': services,
+                            'affected_dependencies': dependency_names,
+                            'affected_services': service_names,
                             'recipients': users,
                             'jira': this.state.jira,
                             'short_description': this.state.short_description,
@@ -223,7 +179,7 @@ class NewNotification extends React.Component {
   }
 
   render() {
-    var { notification_type, selectedDependencies, selectedServices, selectedUsers, selectedSlackUsers, jira, short_description, long_description, validated, optionSelected, userSelected} = this.state;
+    var { notification_type, selectedDependencies, selectedServices, selectedUsers, jira, short_description, long_description} = this.state;
     const animatedComponents = makeAnimated();
     var impacted_users = [];
    
@@ -232,7 +188,7 @@ class NewNotification extends React.Component {
       <Form isHorizontal>
         <FormGroup label="Notification Type" isRequired fieldId="horizontal-form-title">
           <FormSelect 
-            value={this.state.notification_type} 
+            value={notification_type} 
             onChange={e => {
               this.setState({
                 notification_type: e
@@ -280,7 +236,7 @@ class NewNotification extends React.Component {
           fieldId="horizontal-form-title" 
           helperText="Services highlighted in RED are affected by the selected dependency."
         >
-          <Query query={GET_SERVICE_OWNERS}>
+          <Query query={GET_SERVICE_INFO}>
             {({ loading, error, data }) => {
               if (loading) return 'Loading...';
               if (error) return `Error! ${error.message}`;
@@ -290,13 +246,11 @@ class NewNotification extends React.Component {
                 dic["value"] = service.name;
                 dic["label"] = service.name;
                 dic["color"] = GREY;
-                // dic["color"] = "#363636";
                 if (selectedDependencies) {
                   for (var d of selectedDependencies) {
                     if (dependency_dic.hasOwnProperty(d.value)) {
                       if (dependency_dic[d.value].includes(service.name)) {
                         dic["color"] = RED;
-                        // dic["color"] = '#DE350B';
                       }
                     }
                   }
@@ -316,7 +270,7 @@ class NewNotification extends React.Component {
                     });
                   }}
                   allowSelectAll={true}
-                  value={this.state.selectedServices}
+                  value={selectedServices}
                   styles={ColourStyles}
                 />
              }</Fragment>
@@ -324,7 +278,7 @@ class NewNotification extends React.Component {
           </Query>
         </FormGroup>
         <FormGroup label="Recipients" isRequired fieldId="horizontal-form-title">
-          <Query query={GET_SERVICE_OWNERS}>
+          <Query query={GET_SERVICE_INFO}>
             {({ loading, error, data }) => {
               if (loading) return 'Loading...';
               if (error) return `Error! ${error.message}`;
@@ -355,12 +309,12 @@ class NewNotification extends React.Component {
                 var email_dic = {};
                 email_dic["value"] = key;
                 email_dic["label"] = key;
-                email_dic["color"] = GREY;
+                email_dic["color"] = RED;
                 impacted_users.push(email_dic);
               }
               
               return <Fragment>{
-                <CreatableSelect
+                <SelectAll
                   options={sortByValue(impacted_users)}
                   isMulti
                   closeMenuOnSelect={false}
@@ -372,88 +326,13 @@ class NewNotification extends React.Component {
                         });
                       }}
                   allowSelectAll={true}
-                  value={this.state.selectedUsers}           
+                  value={selectedUsers}           
                   styles={ColourStyles}
                 />
              }</Fragment>;
             }}
           </Query>
         </FormGroup>
-        {/* <FormGroup label="Recipients" isRequired fieldId="horizontal-form-title">
-          <Query query={GET_SERVICE_OWNERS}>
-            {({ loading, error, data }) => {
-              if (loading) return 'Loading...';
-              if (error) return `Error! ${error.message}`;
-              var recipients = new Set();
-              if (selectedServices) {
-                for (var key of selectedServices) {
-                  this.selected_service_names.push(key.value);
-                }
-              }
-              for(var key in data.apps_v1) {
-                var service_name = data.apps_v1[key].name;
-                if (this.selected_service_names.includes(service_name)) {
-                  var service_owners = data.apps_v1[key].serviceOwners;
-                  var service_notificators = data.apps_v1[key].serviceNotifications;
-                  if (service_owners) {
-                    for (var k of service_owners) {
-                      recipients.add(k.name);
-                    }
-                  }
-                  if (service_notificators) {
-                    for (var k of service_notificators) {
-                      recipients.add(k.name);
-                    }
-                  }
-                }
-              }
-              for (var key of recipients) {
-                var email_dic = {};
-                email_dic["contact"] = 'Email';
-                if (key in user_dic) {
-                  email_dic["value"] = user_dic[key]['path'] + ' - ' + email_dic['contact'];
-                } else {
-                  email_dic["value"] = key + ' - ' + email_dic['contact'];
-                }
-                // email_dic["value"] = key + ' - ' + email_dic['contact'];
-                email_dic["label"] = key + ' - ' + email_dic['contact'];
-                email_dic["color"] = grey;
-                impacted_users.push(email_dic);
-                var slack_dic = {};
-                slack_dic["contact"] = 'Slack';
-                // slack_dic["value"] = key + ' - ' + slack_dic['contact'];
-                if (key in user_dic) {
-                  slack_dic["value"] = user_dic[key]['slack'] + ' - ' + slack_dic['contact'];
-                } else {
-                  slack_dic["value"] = key + ' - ' + slack_dic['contact'];
-                }
-                slack_dic["label"] = key + ' - ' + slack_dic['contact'];
-                slack_dic["color"] = red;
-                impacted_users.push(slack_dic);
-              }
-              
-              return <Fragment>{
-                <SelectRecepient
-                  options={sortByLabel(impacted_users)}
-                  isMulti
-                  closeMenuOnSelect={false}
-                  hideSelectedOptions={false}
-                  components={{ Option, MultiValue, animatedComponents }}
-                  onChange={e => {
-                        this.setState({
-                          selectedUsers: e 
-                        });
-                      }}
-                  allowSelectAll={true}
-                  value={this.state.selectedUsers}
-                  // getOptionLabel={option => `${option.label} (${option.contact})`}
-                  styles={ColourStyles}
-                  // getOptionValue={option => option['contact'] + option['label']}
-                />
-             }</Fragment>;
-            }}
-          </Query>
-        </FormGroup> */}
         <FormGroup 
           label="Would you like to create a new jira ticket and/ or link to an existing one?" 
           isRequired 
