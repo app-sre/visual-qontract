0f166391f2041ca94634513c84d2aeb8cc9ac078
Ryan Wallace <rywallac@redhat.com>
2022-07-29 15:41:06 -0400

    Optionally remove Nginx proxy server

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -4,4 +4,15 @@ sed -e "s|%AUTHORIZATION%|$AUTHORIZATION|;s|%GRAPHQL_URI%|$GRAPHQL_URI|" \
     -e "s|%API_AUTH%|$API_AUTH|;s|%API_URI%|$API_URI|" \
     /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+if [ "$API_URI" = "disable-qontract-api" ]
+then
+    # Option to remove the proxy server setup for Nginx.
+    # This has the side effect that API calls for creating
+    # Slack notifications in the app will result in frontend errors
+    # and is a quick way to get Visual Qontract working in
+    # environments without Slack access
+    sed "34,37d" /etc/nginx/nginx.conf > /tmp/nginx.conf
+    cat /tmp/nginx.conf > /etc/nginx/nginx.conf
+fi
+
 exec nginx -g "daemon off;"
